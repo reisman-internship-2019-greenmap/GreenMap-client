3c6f599daf20bf77a3003d4c8267a054008d456c
LauraPenney <47276506+LauraPenney@users.noreply.github.com>
2019-04-29 21:59:03 -0300

    Access to camera and swiping component

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
-import { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Swiper from 'react-native-swiper';
+import CameraComponent from './components/cameraComponent';
 
+//TODO: Replace "Hello Swiper" with a main screen menu-like component
+//react-native-swiper Github page: https://github.com/leecade/react-native-swiper
 export default class App extends Component {
-
   render() {
       return (
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
-      </View>
+        <Swiper showsButtons={true}>
+          <View style={styles.helloSwiper}>
+            <Text style={styles.text}>Hello Swiper</Text>
+          </View>
+          <CameraComponent />
+        </Swiper>
     )} //end render
 } //end App
 
 const styles = StyleSheet.create({
-  container: {
+  helloSwiper: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#9DD6EB',
   },
 
-  dummyText: {
-    textAlign: 'center',
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
   }
-});
+})
+
+
