03f85478f59b64e9191484fac03a631216cbf2f8
LauraPenney <47276506+LauraPenney@users.noreply.github.com>
2019-05-09 22:25:34 -0300

    added manual entry button

--- components/scanner.js
+++ components/scanner.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text,} from 'react-native';
-import { Permissions, BarCodeScanner } from 'expo';
+import {StyleSheet, View, Text, Dimensions} from 'react-native';
+import { Permissions, BarCodeScanner, Font } from 'expo';
+import GreenmapButton from './greenmapButton';
 
 export default class Scanner extends Component {
     constructor() {
@@ -8,31 +9,38 @@ export default class Scanner extends Component {
         this.state = {
             hasCameraPermission: null,
             barCodeData: null,
+            fontLoaded: false,
             type: BarCodeScanner.Constants.Type.back,
         };
     } 
    
 
   //If permissions are not set, ask for permission to the user's camera
-  componentDidMount() {
+  //wait for fonts to load and reflect loading in the state
+  async componentDidMount() {
     this._requestCameraPermission();
+    await Font.loadAsync({
+        'raleway-extra-bold': require('../assets/Raleway-ExtraBold.ttf')
+    });
+    this.setState({fontLoaded: true});
   }
 
+  //ask for camera permission and reflect the answer in the state
   _requestCameraPermission = async () => {
     const {status} = Permissions.askAsync(Permissions.CAMERA);
     this.setState({hasCameraPermission: status === "granted"});
   }
 
-
+//helper function for controlling repetative _onScan() calls
 _delay = (time) => {
     return new Promise ((resolve, reject) => {
         setTimeout(() => resolve(), time);
     })
 }
 
-/*Called when a barcode is detected. Note it's called EVERY
-time a detection occurs, not just once per barcode, hence the
-delay*/
+/*Called (repeatidly) when a barcode is detected. If the 
+scanner detects the same code as it did 1/2 a second ago, no
+action is performed. Otherwise the data is processed*/
   _onScan = async scan => {
     await this._delay(500);
     if (this.state.barCodeData == scan.data) return;
@@ -59,14 +67,27 @@ delay*/
                     <View style={styles.focused} />
                     <View style={styles.opaqueEdge} />
                 </View>
-                <View style={styles.opaqueEdge} />
+                <View style={[styles.opaqueEdge, styles.bottom]} />
+                {
+                    this.state.fontLoaded ? (
+                        <GreenmapButton buttonStyles={styles.button}
+                        text="Enter item manually"
+                        textStyles={styles.text} 
+                        onPress={()=> alert("You pressed the button!")}
+                    />
+                    ) : null
+                }
             </BarCodeScanner>
             }
         </View>
     )} //end render
 } //end Scanner
 
+//define consts used in the StyleSheet
 const opacity = 'rgba(0, 0, 0, .6)';
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -83,7 +104,27 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
     },
 
+    bottom: {
+        backgroundColor: opacity,
+    },
+
     focused: {
         flex: 10,   
     },
-}) //end styles
+
+    button: {
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: "#58B34D",
+        position: 'absolute',
+        left: deviceWidth/4,
+        bottom: deviceHeight/10,
+    },
+
+    text: {
+        fontFamily: 'raleway-extra-bold',
+        fontSize: 20,
+        color: '#ffffff',
+        textAlign: 'center'
+    },
+})
