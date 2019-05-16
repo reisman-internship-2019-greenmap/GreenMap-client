d6dd5ff336997cb6d54e8d59bc716960dfc6c6e6
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-05-16 18:13:12 -0300

    refactoring and navigation flow

---
+++ components/controllers/scannerController.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+import { Permissions, Font,} from 'expo';
+import ScannerScreen from '../screens/scannerScreen';
+
+
+export default class ScannerController extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hasCameraPermission: null,
+            barCodeData: null,
+            fontLoaded: false,
+        };
+    } 
+   
+  //If permissions are not set, ask for permission to the user's camera
+  //wait for fonts to load and reflect loading in the state
+  async componentDidMount() {
+    this._requestCameraPermission();
+    await Font.loadAsync({
+        'raleway-extra-bold': require('../../assets/Raleway-ExtraBold.ttf')
+    });
+    this.setState({fontLoaded: true});
+    console.log("Hello from app!") //testing logging
+  }
+
+  //ask for camera permission and reflect the answer in the state
+  _requestCameraPermission = async () => {
+    const {status} = Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission: status === "granted"});
+  }
+
+//helper function for controlling repetative _onScan() calls
+_delay = (time) => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => resolve(), time);
+    })
+}
+
+/*Called (repeatidly) when a barcode is detected. If the 
+scanner detects the same code as it did 1/2 a second ago, no
+action is performed. Otherwise the data is processed*/
+  _onScan = async scan => {
+    await this._delay(500);
+    if (this.state.barCodeData == scan.data) return;
+    this.setState({barCodeData: scan.data});
+
+    //navigate to results screen
+    this.props.navigation.navigate("Results");
+  }
+
+  onFormButtonPress = () => {
+      this.props.navigation.navigate("Form");
+  }
+
+  //Return camera view with scanner implemented if permission is given
+  render() {
+     return (
+        <View style={{flex:1}}>
+            {this.state.hasCameraPermission === null?
+                <Text>Requesting camera access</Text>:
+            this.state.hasCameraPermissionn === false?
+                <Text>You must enable camera access to scan a barcode</Text>:
+            <ScannerScreen
+            onScan={this._onScan}
+            isFontLoaded={this.state.fontLoaded}
+            onFormButtonPress={this.onFormButtonPress} />
+            }
+        </View>
+    )} //end render
+} //end Scanner
