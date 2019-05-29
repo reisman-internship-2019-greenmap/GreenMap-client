9aba34af51ebb8fb4ddd49938828f5b2e9aa2bbb
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-05-29 19:48:54 -0300

    component state resets after user navigates away

--- src/containers/scannerContainer.js
+++ src/containers/scannerContainer.js
@@ -2,7 +2,8 @@ import {connect} from 'react-redux';
 import Scanner from '../components/scanner';
 
 const mapStateToProps = (state) => ({
-    onScanCalls: state.ScannerInfo.onScanCalls
+    onScanCalls: state.ScannerInfo.onScanCalls,
+    barcodeData: state.ScannerInfo.barcodeData,
 })
 
 export default ScannerContainer = connect(mapStateToProps)(Scanner)
