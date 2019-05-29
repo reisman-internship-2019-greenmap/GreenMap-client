ccb06485529b0bc0509618b6cdbcc0f81a0238d2
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-05-29 19:47:48 -0300

    component state resets after user navigates away

--- src/components/scanner.js
+++ src/components/scanner.js
@@ -17,29 +17,62 @@ class ScannerScreen extends Component {
         }
 
     }
-
+    //TODO: Factor in camera permissions
     async componentDidMount() {
         await Font.loadAsync({
             "Raleway-Extra-Bold": require("../../assets/Raleway-ExtraBold.ttf")
         });
         this.setState({fontsLoaded : true});
-        this.listener = this.props.navigation.addListener("didFocus", () => {
+        this.listener = this.props.navigation.addListener("willFocus", () => {
             this.resetValidation();
         })
-    }
+    } //
 
     resetValidation = () => {
-        this.setState({stateScanCalls: 0})
-        console.log("Reset validation was called");
+        this.props.dispatch({type: "RESET_CALL_TRACKER"});
+        this.setState({onBarcodeScan: this._onScan}, () => {
+            console.log(`Reset validation was called, onScanCalls is ${this.props.onScanCalls}`);
+        })
+        
     }
 
   _onScan = (scan) => {
+      console.log(`onScan was called and scan calls is ${this.props.onScanCalls}`);
       this.props.dispatch({type: "UPDATE_CALL_TRACKER"});
-      console.log(this.props.onScanCalls);
-
-      if (this.props.onScanCalls == 5) {
-        this.setState({onBarcodeScan: undefined}, () => this.props.navigation.navigate("ResultsScreen"))
+      
+      //set initial read
+      if (this.props.onScanCalls == 0) {
+          this.props.dispatch({type: "UPDATE_BARCODE_DATA", data:scan.data});
+          return
       }
+
+      //if this condition is met then the barcode is valid
+      else if (this.props.onScanCalls == 5) {
+        this.setState({onBarcodeScan: undefined}, 
+            () => {
+            fetch('https://facebook.github.io/react-native/movies.json')
+            .then((res) => res.json())
+            .then((resJSON) => {
+                if (resJSON.title == "The Basics - Networking") {
+                    this.props.dispatch({type: "RENDER_BARCODEMISS_SCREEN"});
+                }
+            }) //end callback
+            this.props.navigation.navigate("ResultsScreen");
+            })
+        return
+      }
+
+      //compare the current read and the previous read
+      else {
+          if (scan.data != this.props.barcodeData) {
+              console.log(`in else block. scan calls is ${this.props.onScanCalls}`);
+              this.props.dispatch({type:"RESET_CALL_TRACKER"});
+              console.log(`Validation error, onScanCalls is ${this.props.onScanCalls}`);
+              return
+          }
+      }
+      
+      console.log(`End of onScan function. barcodeData is ${this.props.barcodeData}`);
 }
     
   goToForm = () => {
