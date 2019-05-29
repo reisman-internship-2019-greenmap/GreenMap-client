b32caeef06da59423ded1b056897d170ebf8252e
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-05-29 19:49:39 -0300

    component state resets when user navigates away

--- src/redux/reducers/scannerReducer.js
+++ src/redux/reducers/scannerReducer.js
@@ -1,11 +1,29 @@
+//anything related to the scanner goes here
 const initialState = {
-    onScanCalls: 0
+    onScanCalls: 0,
+    barcodeData: null,
 }
 
+const barcodeDataTracker = (state=initialState.barcodeData, action) => {
+    switch(action.type) {
+        case "UPDATE_BARCODE_DATA":
+            console.log(`action recieved, data is ${action.data}`);
+            return action.data
+        
+            default:
+            return state
+    }
+}
+
+//call tracker for validation process
 const callTracker = (state=initialState.onScanCalls, action) => {
     switch(action.type) {
         case "UPDATE_CALL_TRACKER":
             return state + 1
+
+        case "RESET_CALL_TRACKER":
+            console.log("RESET_CALL_TRACKER action was dispatched");
+            return 0
 
         default:
             return state
@@ -16,6 +34,12 @@ const ScannerReducer = (state=initialState, action) => {
     switch(action.type) {
         case "UPDATE_CALL_TRACKER":
             return {...state, onScanCalls: callTracker(state.onScanCalls, action)}
+
+        case "RESET_CALL_TRACKER":
+            return {...state, onScanCalls: callTracker(state.onScanCalls, action)}
+
+        case "UPDATE_BARCODE_DATA":
+            return {...state, barcodeData: barcodeDataTracker(state.barcodeData, action)}
 
         default:
             return state
