11512e35659d705953223dc99f39e74a4611231c
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-05-29 21:05:28 -0300

    moved permissions from local state to redux

--- src/redux/reducers/scannerReducer.js
+++ src/redux/reducers/scannerReducer.js
@@ -2,8 +2,10 @@
 const initialState = {
     onScanCalls: 0,
     barcodeData: null,
+    permissions: null,
 }
 
+//keeps track of currently read barcode data
 const barcodeDataTracker = (state=initialState.barcodeData, action) => {
     switch(action.type) {
         case "UPDATE_BARCODE_DATA":
@@ -30,6 +32,19 @@ const callTracker = (state=initialState.onScanCalls, action) => {
     }
 }
 
+//keeps track of permission statuses
+const permissionsTracker = (state=initialState.permissions, action) => {
+    switch(action.type) {
+        case "UPDATE_CAMERA_PERMISSIONS":
+            console.log('Camera permissions action dispatched');
+            return action.status
+        
+        default:
+            return state
+    }
+}
+
+//combines all above reducers
 const ScannerReducer = (state=initialState, action) => {
     switch(action.type) {
         case "UPDATE_CALL_TRACKER":
@@ -40,6 +55,9 @@ const ScannerReducer = (state=initialState, action) => {
 
         case "UPDATE_BARCODE_DATA":
             return {...state, barcodeData: barcodeDataTracker(state.barcodeData, action)}
+
+        case "UPDATE_CAMERA_PERMISSIONS":
+            return {...state, permissions: permissionsTracker(state.permissions, action)}
 
         default:
             return state
