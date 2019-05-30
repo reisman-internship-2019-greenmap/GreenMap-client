b16e4611966515a7461cb7647003fb2c49275b7a
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-05-29 21:04:46 -0300

    added permissions to mapStateToProps

--- src/containers/scannerContainer.js
+++ src/containers/scannerContainer.js
@@ -4,6 +4,7 @@ import Scanner from '../components/scanner';
 const mapStateToProps = (state) => ({
     onScanCalls: state.ScannerInfo.onScanCalls,
     barcodeData: state.ScannerInfo.barcodeData,
+    permissions: state.ScannerInfo.permissions,
 })
 
 export default ScannerContainer = connect(mapStateToProps)(Scanner)
