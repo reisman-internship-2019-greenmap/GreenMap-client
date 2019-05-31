765a5907ba03139da31505b95bc96b435fe11fe5
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-05-31 15:20:37 -0300

    updated mapStateToProps
    mapStateToProps now includes the "result" object containing info from the server.

--- src/containers/resultContainer.js
+++ src/containers/resultContainer.js
@@ -2,8 +2,9 @@ import {connect} from 'react-redux';
 import ResultsView from '../components/results';
 
 const mapStateToProps = (state) => ({
-    didWikiMiss: state.resultStatus.didWikiMiss,
-    didBarcodeMiss: state.resultStatus.didBarcodeMiss,
+    didWikiMiss: state.resultInfo.didWikiMiss,
+    didBarcodeMiss: state.resultInfo.didBarcodeMiss,
+    resultDoc: state.resultInfo.result,
 })
 
 export default ResultsContainer = connect(mapStateToProps)(ResultsView)
