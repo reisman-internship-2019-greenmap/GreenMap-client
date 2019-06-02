ad17eb44612db8ab7dd087b78aae4b23c68170c3
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-06-02 18:48:47 -0300

    added formReducer (supplied by redux)

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import ScannerReducer from './scannerReducer';
 import ResultReducer from './resultReducer';
+import {reducer as FormReducer} from 'redux-form';
 
 const RootReducer = combineReducers({
     ScannerInfo: ScannerReducer,
     resultInfo: ResultReducer,
+    FormInfo: FormReducer,
 })
 
 export default RootReducer
