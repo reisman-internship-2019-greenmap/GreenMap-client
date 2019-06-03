9573e78ea78a561e444c0f3d2eb34c430bf41dbe
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-06-03 20:09:01 -0300

    updated global styles

--- appStyle.js
+++ appStyle.js
@@ -7,8 +7,8 @@ export default StyleSheet.create({
     app_button_light: {
         backgroundColor: "#58B34D",
         padding: 10,
-        borderRadius: 5,
-        borderColor: "#3D3D3D",
+        borderRadius: 8,
+        borderColor: "black",
         borderWidth: 1,
     },
 
@@ -17,5 +17,12 @@ export default StyleSheet.create({
         fontSize: 20,
         color: '#ffffff',
         textAlign: 'center',
+    },
+
+    app_text_reg: {
+        fontFamily: 'Raleway-regular',
+        fontSize: 18,
+        color: '#44963A',
+        textAlign: 'left',
     }
 })
