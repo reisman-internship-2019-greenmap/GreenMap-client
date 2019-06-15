b54207f0e5d66bc45abd214201b829742949f4a3
Lee Penney <47276506+LeeMPenney@users.noreply.github.com>
2019-06-15 19:53:00 -0300

    styling

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -5,14 +5,45 @@ import {
     TouchableOpacity, 
     StyleSheet,
     View
-} from 'react-native'; 
+} from 'react-native';
 
+import Modal from 'react-native-modal';
 import app_styles from '../../../appStyle';
+import {Ionicons} from '@expo/vector-icons'
 
 import EntryFormInput from './formInput';
 
-/*It is easier to use functions rather than classes 
-for redux-form construction*/
+/**  
+ * @MARK FORM VALIDATION
+ * The values returned by these functions are
+ * displayed underneath the field, visible to the
+ * user.
+ * 
+ * @required
+ * checks if a field value is empty
+ * 
+ * @isABarcode
+ * checks that the barcode field contains only
+ * numbers and that is is 12 (UPC) or 13 (EAN-13)
+ * digits in length
+ */
+const required = value => value ? undefined : "This field is required"
+const isABarcode = value => {
+    if ((Number(value) !== NaN) & (value.length == 12 || value.length == 13)) {
+        return undefined
+    }
+    return "That is not a valid barcode"//
+}
+
+/**
+ * @MARK MANUAL ENTRY FORM
+ * this is a redux form and much
+ * of the functionality is automatically supplied through
+ * redux. This includes the {handleSubmit} and {onChange}
+ * props
+ */
+
+
 const ManualEntryForm = (props) => {
     //Extract the handleSubmit prop supplied by redux-form
     const {handleSubmit} = props;
@@ -21,6 +52,7 @@ const ManualEntryForm = (props) => {
     const submitValues = (values) => {
         body = JSON.stringify(values);
         console.log(body);
+        
         /*fetch('192.168.1.169:/3000', {
             method: "POST",
             headers: {
@@ -33,7 +65,7 @@ const ManualEntryForm = (props) => {
         .then(resJSON => console.log(resJSON)) */
       }
      
-     //Render the form
+     //Render the form.
      return (
         <View style={[styles.container, {paddingTop: 30}]}>
             <Text
@@ -41,29 +73,42 @@ const ManualEntryForm = (props) => {
             {fontSize:20, color: "#58B34D", paddingBottom: 30, textDecorationLine: "underline"}]}>
                 Enter Product Information Below
             </Text>
+            <Modal isVisible={true}>
+                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                  <View style={{alignItems: "center", justifyContent: "center", backgroundColor:"white"}}>
+                      <Text style={[app_styles.app_text_bold, {padding: 20, textAlign: "center", color: "#58B34D", fontSize: 45 }]}>Submitted!</Text>
+                      <Ionicons name="md-checkbox-outline" size={90} color="#CCCCCC" />
+                      <TouchableOpacity
+                        style={[app_styles.app_button_light, {marginTop: 30, marginBottom: 20, paddingLeft: 30, paddingRight: 30}]} 
+                        onPress={() => {alert("you pressed the button!")}}>
+                        <Text style={[app_styles.app_text_bold, {fontSize: 22}]}>Ok</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </Modal>
             <KeyboardAwareScrollView
                 extraScrollHeight={100}>
-                <View style={[styles.container, {marginTop: 10}]}>
+                <View style={styles.container}>
                 <EntryFormInput 
                     title="Barcode Number" 
                     fieldName="barcode"
-                    placeholder="Ex: 0039800079305"/>
+                    placeholder="Ex: 0039800079305"
+                    validate={[required, isABarcode]}/>
                 <EntryFormInput 
                     title="Product Name" 
                     fieldName="product_name"
-                    placeholder="Dell G7 Gaming Laptop" />
+                    placeholder="Dell G7 Gaming Laptop"
+                    validate={[required]} />
                 <EntryFormInput 
                     title="Product Category" 
                     fieldName="category"
-                    placeholder="Laptop" />
+                    placeholder="Laptop"
+                    validate={[required]} />
                 <EntryFormInput 
                     title="Manufacturer" 
                     fieldName="manufacturer"
-                    placeholder="Dell Canada" />
-                <EntryFormInput 
-                    title="ESG Score" 
-                    fieldName="ESG"
-                    placeholder="4.2" />
+                    placeholder="Dell Canada"
+                    validate={[required]} />
                 <TouchableOpacity
                 style={[app_styles.app_button_light, {marginTop: 30, paddingLeft: 20, paddingRight: 20}]} 
                 onPress={handleSubmit(submitValues)}>
--- src/components/form/formInput.js
+++ src/components/form/formInput.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import {Text, View, TextInput, StyleSheet} from 'react-native';
 import {Field} from 'redux-form';
+import {Ionicons} from '@expo/vector-icons'
 
 import app_styles from '../../../appStyle';
 
 //Stateless functions are the best components for redux form inputs
 const renderInput = (props) => {
-  const {input, ...inputProps} = props;
-  return <TextInput 
-    {...inputProps}
-    style={styles.input} 
-    onChangeText={input.onChange}
-    value={input.value}
-    returnKeyType="done"
-    placeholder={props.placeholder} 
-    />
-  }
+  //
+  const {input, meta, ...inputProps} = props;
+  return ( <View style={{flexDirection: "column", alignItems: "flex-start"}}>
+            <TextInput 
+           {...inputProps}
+            style={styles.input} 
+            onChangeText={input.onChange}
+            value={input.value}
+            returnKeyType="done"
+            placeholder={props.placeholder} />
+              {meta.touched && meta.error ?
+              <View style={{flexDirection: "row", alignItems: "center"}}> 
+              <Ionicons name="md-alert" size={20} color="#E24747" />
+              <Text style={styles.error}> {meta.error} </Text>
+              </View> :
+              <Text> </Text>}
+    </View>
+  )
+}
 
 //Components need to be wrapped in a Field for redux-form to work properly
 const EntryFormInput = (props) => {
     return (
-        <View>
+        <View style={{flex: 1, flexDirection: "column", justifyContent: "flex-start", margin: 6 }}>
             <Text style={[app_styles.app_text_bold, {color: '#44963A', textAlign: "left"}]}>
             {props.title}
             </Text>
@@ -28,6 +38,7 @@ const EntryFormInput = (props) => {
             name={props.fieldName}
             component={renderInput}
             placeholder={props.placeholder}
+            validate={props.validate}
             />
         </View>
     )
@@ -42,6 +53,11 @@ const styles = StyleSheet.create({
       borderWidth: 1,
       height: 40,
       padding: 10,
-      marginBottom: 20,
+    },
+
+    error: {
+      fontSize: 13,
+      color: "#E24747",
+      fontWeight: "bold"
     }
   });
