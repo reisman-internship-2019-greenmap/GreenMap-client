b53eda38a13dd2c59abdf0ec3aab4020d25459a5
Laura Penney <laurapenney007@gmail.com>
2019-09-08 11:42:01 -0300

    fetches from both endpoints (form)

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -12,7 +12,7 @@ import AppStyles from '../../globals/styles/AppStyle';
 import FormStyles from './FormStyles';
 
 import {Ionicons} from '@expo/vector-icons'
-import getProductInfo from '../../utils/networking';
+import {getProductInfo, getTopFive } from '../../utils/networking';
 
 
 
@@ -56,21 +56,30 @@ class ManualEntryForm extends Component {
         this.setState({barcodeError: error}, () => {
             if (this.state.barcodeError == null) {
                 getProductInfo(this.state.barcodeValue)
-                .then(res => {
-                    console.log(`res is ${JSON.stringify(res)}`)
-                    if (res === "The connection timed out") {
-                        this.props.dispatch({type: "RESULT_FAILURE", payload: res})
+                .then(getProductRes => {
+                    console.log(`Form.js: getProductRes is ${JSON.stringify(getProductRes)}`)
+                    if (getProductRes === "The connection timed out") {
+                        console.log("Line inside if connection timed out")
+                        this.props.dispatch({type: "RESULT_ERROR", payload: getProductRes})
                     }
 
                     else {
-                        this.props.dispatch({type: "UPDATE_RESULT", result: res})
+                        console.log("Line before getTopFive")
+                        getTopFive(this.state.barcodeValue)
+                        .then(topFiveRes => {
+                            console.log(`Form.js: got ${topFiveRes} from second endpoint`)
+                            console.log(`First res is still ${getProductRes}`)
+                            //compose final response object here
+                            //then send it to store
+                            this.props.dispatch({type: "UPDATE_RESULT", result: getProductRes})
+                        })  
                     }
-                }
-                )
+                })
                 .catch(err => {
-                    console.log(`Error! ${err}`);
-                    this.props.dispatch({type: "RESULT_FAILURE", payload: err})
-                }) 
+                    console.log(`Form.js: Error! ${err}`);
+                    this.props.dispatch({type: "RESULT_ERROR", payload: err})
+                })
+                console.log("Line before results navigation") 
                 this.props.navigation.navigate("ResultsScreen");
              //end server communication
             }
